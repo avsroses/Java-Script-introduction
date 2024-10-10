4056a8367ebfd89de3c16c3d6f085c537c2bb1a4
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-10 15:51:44 +0100

    button click changes image

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 // Variables
 const myButton = document.getElementById("my-button");
 const myParagraph = document.getElementById("my-paragraph");
+const characterImage = document.getElementById("image-1");
 
 let clickCounter = 0;
 
 function clickFunction() {
 
+    // Adds to counter and outputs to console
     console.log('The button has been clicked');
     clickCounter++;
 
     // Changes text without changing it in the HTML
     myParagraph.innerHTML = "I have been clicked " + clickCounter + " time(s)";
+    // Changes image beinmg displayed
+    characterImage.src = "assets/Wolf.webp";
 }
 
 // Two methods - both do the same
