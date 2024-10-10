d6d5a1f87bd98b5f5d7d205803a1a6fbc7af99b8
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-10 15:21:25 +0100

    added click counter instead of text

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
+// Variables
 const myButton = document.getElementById("my-button");
 const myParagraph = document.getElementById("my-paragraph");
 
+let clickCounter = 0;
+
+function clickFunction() {
+
+    clickCounter = clickCounter+1;
+
+    // Changes text without changing it in the HTML
+    myParagraph.innerHTML = "I have been clicked " + clickCounter + " time(s)";
+}
 
 // Two methods both do the same
 // document.getElementById("my-button").addEventListener("click", clickFunction);
 // document.getElementById("my-button").onclick = clickFunction;
 myButton.onclick = clickFunction;
-
-function clickFunction() {
-    // Changes text without changing it in the HTML
-    myParagraph.innerHTML = "new paragraph";
-}
