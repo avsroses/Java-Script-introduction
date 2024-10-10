1b89e659f4b8d40b60c44ff74c88624c2673a3ce
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-10 14:53:55 +0100

    simplifying code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+const myButton = document.getElementById("my-button");
+const myParagraph = document.getElementById("my-paragraph");
+
+
 // Two methods both do the same
 // document.getElementById("my-button").addEventListener("click", clickFunction);
-document.getElementById("my-button").onclick = clickFunction;
-
+// document.getElementById("my-button").onclick = clickFunction;
+myButton.onclick = clickFunction;
 
 function clickFunction() {
     // Changes text without changing it in the HTML
-    document.getElementById("my-paragraph").innerHTML = "new paragraph";
+    myParagraph.innerHTML = "new paragraph";
 }
