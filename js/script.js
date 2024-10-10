26c93992506ac821e56084ed8efb8a6c4894b575
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-10 15:37:51 +0100

    comments

--- js/script.js
+++ js/script.js
@@ -6,13 +6,14 @@ let clickCounter = 0;
 
 function clickFunction() {
 
-    clickCounter = clickCounter+1;
+    console.log('The button has been clicked');
+    clickCounter++;
 
     // Changes text without changing it in the HTML
     myParagraph.innerHTML = "I have been clicked " + clickCounter + " time(s)";
 }
 
-// Two methods both do the same
+// Two methods - both do the same
 // document.getElementById("my-button").addEventListener("click", clickFunction);
 // document.getElementById("my-button").onclick = clickFunction;
 myButton.onclick = clickFunction;
