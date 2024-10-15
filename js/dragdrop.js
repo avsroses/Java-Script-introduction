1a6fe94f8c7e4c992c1fcfa16f54017b69af0e4f
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-15 14:39:26 +0100

    drap and drop image

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -1,16 +1,19 @@
 const fancyHat = document.getElementById("complement-one");
-const dropZone = document.getElementById ("character-img");
+const dropZone = document.getElementById("character-image");
 
-function onDrop(event) { 
+function onDrop(event) {
+    fancyHat.style.left = event.clientX + "px";
+    fancyHat.style.top = event.clientY + "px";
     console.log("Element has been dropped");
 }
 
 function onDragOver(event) {
+    event.preventDefault();
     console.log("Something is being dragged over me!");
 }
 
 function onDragStart() {
-    console.log ("I'm being dragged")
+    console.log("I'm being dragged")
 }
 
 dropZone.ondrop = onDrop;
