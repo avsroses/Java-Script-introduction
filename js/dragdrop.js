575bb359b7322da9156759f5d981342531a343a8
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-15 16:10:10 +0100

    comments

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -4,8 +4,15 @@ const complements = document.getElementsByClassName("complement")
 // dropzone is the area where compliments can be dropped
 const dropZone = document.getElementById("character-image");
 
+// offsetX and offsetY store the initial offset from clicking on the dragged image
+let offsetX = 0;
+let offsetY = 0;
+
+// draggedImage stores the currently dragged image
+let draggedImage = undefined;
+
 /**
- * When something is dropped in the drop zone, update draggedImage style accordingly
+ * when something is dropped in the drop zone, update draggedImage style accordingly
  * @param {Event} event stores information on the current drop event
  */
 function onDrop(event) {
@@ -24,15 +31,23 @@ function onDragOver(event) {
     event.preventDefault();
 }
 
-let offsetX = 0;
-let offsetY = 0;
-let draggedImage = undefined;
-
+/**
+ * store draggedImage as the event target and compute initial offsetX and offsetY
+ * @param {Event} event stores information related to the current drag start event
+ */
 function onDragStart(event) {
+    // event.target points to the HTML Element that started the drag event, i.e
+    // the currently dragged image
     draggedImage = event.target;
 
+    // window.getComputedStyle returns the style of the element removing the css text
+    // for ex. "100px" becomes "100"
     const style = window.getComputedStyle(draggedImage, null);
 
+    // offset is calculated as:
+    // clientX (horizontal mouse position) - style.left (left position of image)
+    // clientY (vertical mouse position) - style.top (top position of the image)
+    // parseInt is needed to transform the style os trings to numbers
     offsetX = event.clientX - parseInt(style.left);
     offsetY = event.clientY - parseInt(style.top);
 }
