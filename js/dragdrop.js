b6a100dfb01f2240c210f70a97488c9269cdd1a0
avsroses <ava.wilkinson@student.norwichuni.ac.uk>
2024-10-15 16:02:04 +0100

    commenting and removal of console logs

--- js/dragdrop.js
+++ js/dragdrop.js
@@ -1,16 +1,27 @@
+//store HTML elements of class complement in complements
 const complements = document.getElementsByClassName("complement")
+
+// dropzone is the area where compliments can be dropped
 const dropZone = document.getElementById("character-image");
 
+/**
+ * When something is dropped in the drop zone, update draggedImage style accordingly
+ * @param {Event} event stores information on the current drop event
+ */
 function onDrop(event) {
-    // Bring complement to the position (ClinetX, ClientY)
+    // update draggedImage style to: 
+    //event.clientX (horizontal mouse position) - offsetX (calculated initial offset)
+    //event.clientY (vertical mouse position) - offsetY (calculated initial offset)
     draggedImage.style.left = event.clientX - offsetX + "px";
     draggedImage.style.top = event.clientY - offsetY + "px";
-    console.log("Element has been dropped");
 }
 
+/**
+ * prevent the event default to allow dropping to happen
+ * @param {Event} event tores information on the current drag over event
+ */
 function onDragOver(event) {
     event.preventDefault();
-    console.log("Something is being dragged over me!");
 }
 
 let offsetX = 0;
@@ -24,8 +35,6 @@ function onDragStart(event) {
 
     offsetX = event.clientX - parseInt(style.left);
     offsetY = event.clientY - parseInt(style.top);
-
-    console.log("I'm being dragged");
 }
 
 dropZone.ondrop = onDrop;
